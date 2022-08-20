b705823928d6dde2cafaaf1c811de317fd9fb10d
mbkhalid23 <mbkhalid23@gmail.com>
2022-08-21 00:23:27 +0500

    Add bootswatch theme.

--- CRUDApplication-MVC/Controllers/CategoryController.cs
+++ CRUDApplication-MVC/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDApplication_MVC.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDApplication_MVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
