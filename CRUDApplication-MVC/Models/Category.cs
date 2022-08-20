f9e604ade64e27d64f27cd0a57651516ea7c285e
mbkhalid23 <mbkhalid23@gmail.com>
2022-08-20 19:35:57 +0500

    Create Category model.

---
+++ CRUDApplication-MVC/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApplication_MVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+
+    }
+}
